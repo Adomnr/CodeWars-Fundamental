bfb42f3e228a2187788b7c90ab0486b7fc40ee84
Memory <59786843+Adomnr@users.noreply.github.com>
2023-02-14 17:45:38 +0500

    Create Return Negative.cpp

---
+++ Return Negative.cpp
@@ -0,0 +1,15 @@
+int makeNegative(int num)
+{
+  if (num >0)
+    return -num
+  return num;
+}
+/*
+Describe(make_negative_algorithm)
+{
+    It(should_handle_positive_test)
+    {
+        Assert::That(makeNegative(42), Equals(-42));
+    }
+};
+*/
